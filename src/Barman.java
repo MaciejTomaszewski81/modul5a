77c7d82532f3ac7b6078a5dbe22817bc54fa19dd
soho.tomaszewski@gmail.com <Debiledwa2>
2020-10-15 15:42:04 +0200

    Działający Bar

--- src/Barman.java
+++ src/Barman.java
@@ -1,10 +1,12 @@
 public class Barman {
     void order(Drink drink, Person person) {
-        if (drink.isAlcohol() == true) {
-            if (person.getAge() >= 18) {
+        if (drink.isAlcohol() && person.getAge() >=18) {
                 System.out.println(person.getName() + ", Twój drink jest gotowy do odbioru. Kwota do zapłaty: " + drink.getPrice() + " zł");
-            } else System.out.println("Nie masz 18 lat, nie pijesz alkoholu");
-        } else System.out.println("Smacznego drinka");
+           }  else if (drink.isAlcohol() && person.getAge() < 18) {
+            System.out.println("Nie masz 18 lat, nie pijesz alkoholu");
+        } else {
+            System.out.println("Smacznego drinka");
+        }
     }
 }
 
