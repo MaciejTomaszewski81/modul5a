c333cdd4890aac6b46302efe5ed8cd63d47059a6
soho.tomaszewski@gmail.com <Debiledwa2>
2020-10-15 20:57:25 +0200

    Poprawione ograniczenie waunków

--- src/Bar.java
+++ src/Bar.java
@@ -1,7 +1,7 @@
 public class Bar {
     public static void main(String[] args) {
-        Person person = new Person("Ellen", "Ripley", 28);
-        Drink drink = new Drink("Rum z colą", 15, false);
+        Person person = new Person("Ellen", "Ripley", 27);
+        Drink drink = new Drink("Rum z colą", 15, true);
         Barman barman = new Barman();
         barman.order(drink, person);
     }
--- src/Barman.java
+++ src/Barman.java
@@ -1,9 +1,9 @@
 public class Barman {
     void order(Drink drink, Person person) {
-        if (drink.isAlcohol() && person.getAge() >=18) {
-                System.out.println(person.getName() + ", Twój drink jest gotowy do odbioru. Kwota do zapłaty: " + drink.getPrice() + " zł");
-           }  else  {
-            System.out.println("Nie masz 18 lat, nie pijesz alkoholu");
+        if (drink.isAlcohol() && person.getAge() < 18) {
+            System.out.println("Nieletnim alkoholu nie sprzedajemy");
+        } else {
+            System.out.println(person.getName() + " Twój drink jest gotowy do odbioru Kwota do zapłaty: " + drink.getPrice() + " zł");
         }
     }
 }
