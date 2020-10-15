82b25b5417e42f37e7e14e6eff6a540c6e46ce2e
soho.tomaszewski@gmail.com <Debiledwa2>
2020-10-15 20:20:23 +0200

    Poprawa bez else if

--- src/Bar.java
+++ src/Bar.java
@@ -1,7 +1,7 @@
 public class Bar {
     public static void main(String[] args) {
-        Person person = new Person("Ellen", "Ripley", 17);
-        Drink drink = new Drink("Rum z colą", 15, true);
+        Person person = new Person("Ellen", "Ripley", 28);
+        Drink drink = new Drink("Rum z colą", 15, false);
         Barman barman = new Barman();
         barman.order(drink, person);
     }
--- src/Barman.java
+++ src/Barman.java
@@ -2,10 +2,8 @@ public class Barman {
     void order(Drink drink, Person person) {
         if (drink.isAlcohol() && person.getAge() >=18) {
                 System.out.println(person.getName() + ", Twój drink jest gotowy do odbioru. Kwota do zapłaty: " + drink.getPrice() + " zł");
-           }  else if (drink.isAlcohol() && person.getAge() < 18) {
+           }  else  {
             System.out.println("Nie masz 18 lat, nie pijesz alkoholu");
-        } else {
-            System.out.println("Smacznego drinka");
         }
     }
 }
